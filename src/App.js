81d822295c4626a6c72bfab428190ca0e4faa6dc
MhmdSyd <mhmdsyd.workout@gmail.com>
2023-03-26 13:23:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, { Component } from 'react'
+import Switch from 'react-switch'
+// import Child from './Child'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false,
+      name : "",
+      age : 25,
+      readonly: "",
+      clear: "Cleared"
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+
+  changeName = (e)=>{
+    if(e.target.value===""){
+      this.setState({clear: "Cleared"})
+    }else{
+      this.setState({clear: "there is text value."})
+    }
+
+    this.setState({
+      name: e.target.value
+    })
+
+  }
+
+  // Clear = (e)=>{
+    
+  //   this.setState({
+  //     name: ""
+  //   })
+  // }
+
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    this.setState({
+      name: "",
+      clear: "Cleared"
+    })
+    document.getElementById("inp-txt1").value = ""
+    
+  }
+
+  handleChange(e) {
+    this.setState({ checked: e });
+    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form className="body-form" onSubmit={this.handleSubmit}>
+          <Switch onChange={this.handleChange} checked={this.state.checked} />
+          <br/>
+          <div className="empty-div"></div>
+          <input id="inp-txt1" className="inp-txt" onChange={this.changeName} readOnly={!this.state.checked}/>
+          <button className="btn-submit">Clear</button>
+          <br/>
+          {this.state.name}
+          <br/>
+          <p>{this.state.clear}</p>
+        </form>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     name : "Mohamed",
+//     age : 26
+//   }
+
+//   change = ()=>{
+//     this.setState({
+//       name : "Omar",
+//       age : this.state.age + 1
+//     })
+//   }
+//   render () {
+//     return (
+//         <div className="App">
+//           <button onClick={this.change} onDoubleClick={()=>{console.log(`Age = ${this.state.age}`)}}>Change</button>
+//           <Child name={this.state.name} age={this.state.age}/>
+//         </div>
+//     )
+//   }
+// }
+
+export default App
